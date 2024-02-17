0cdf07b35d5f60cecb6a45de776963c8bf2968ac
nathika <nathika@opendream.co.th>
2024-02-17 19:05:31 +0700

    Add BankTransferNote and implement test cases for BankTransferDonation

--- domain/bank_transfer_donation.py
+++ domain/bank_transfer_donation.py
@@ -1,27 +1,62 @@
 from datetime import datetime
 from enum import Enum
+from typing import Any
+
 from domain.donee import Donee
 from domain.donor import Donor
 from domain.money import Money
 
 
 class BankTransferDonationStatus(Enum):
-    New = "NEW"
-    Pending = "PENDING"
-    Paid = "PAID"
+    NEW = "new"
+    PENDING = "pending"
+    PAID = "paid"
 
 
 class BankTransferNote:
-    def __init__(self, note: str, amount: Money, date: datetime):
+    def __init__(
+        self,
+        note: str,
+        amount: Money,
+        date: datetime,
+        id: int = None,
+    ):
+        self.id = id  # this will exist after the note is saved
         self.note = note
         self.amount = amount
         self.date = date
 
     def __eq__(self, other):
         return (
-            self.note == other.note
+            self.id == other.id
+            and self.note == other.note
             and self.amount == other.amount
             and self.date == other.date
+        )
+
+
+class BankTransferTransaction:
+    def __init__(
+        self,
+        transaction_id: str,
+        date: datetime,
+        amount: Money,
+        note_id: int,
+        id: int = None,
+    ):
+        self.id = id  # this will exist after the transaction is saved
+        self.transaction_id = transaction_id
+        self.date = date
+        self.amount = amount
+        self.note_id = note_id
+
+    def __eq__(self, other):
+        return (
+            self.id == other.id
+            and self.transaction_id == other.transaction_id
+            and self.date == other.date
+            and self.amount == other.amount
+            and self.note_id == other.note_id
         )
 
 
@@ -33,22 +68,27 @@ class BankTransferDonation:
         expected_amount: Money,
         donor: Donor,
         donee: Donee,
+        notes: list[BankTransferNote] = [],
+        transactions: list[BankTransferTransaction] = [],
+        status: BankTransferDonationStatus = BankTransferDonationStatus.NEW,
+        form_data: dict[str, Any] = {},
+        meta: dict[str, Any] = {},
     ):
         self.id = id
         self.donation_number = donation_number
         self.expected_amount = expected_amount
         self.donor = donor
         self.donee = donee
-        self.notes = []
-        self.transactions = []
-        self.status = BankTransferDonationStatus.New
-        self.form_data = {}
-        self.meta = {}
+        self.notes = notes
+        self.transactions = transactions
+        self.status = status
+        self.form_data = form_data
+        self.meta = meta
 
     def add_note(self, note: str, amount: Money, date: datetime):
         if amount == 0:
             raise ValueError("Amount must be greater than 0")
-        
+
         if not self.transactions:
             note = BankTransferNote(
                 note=note,
@@ -56,6 +96,47 @@ class BankTransferDonation:
                 date=date,
             )
             self.notes.append(note)
-            self.status = BankTransferDonationStatus.Pending
+            self.status = BankTransferDonationStatus.PENDING
         else:
             raise ValueError("Donation already has transactions")
+
+    def validate_note_id(self, note_id: int):
+        is_valid = False
+        for note in self.notes:
+            if note.id == note_id:
+                is_valid = True
+
+        if not is_valid:
+            raise ValueError("Note ID must be one of the Donation's Notes")
+
+        return is_valid
+
+    def confirm_transaction(
+        self,
+        transaction_id: int,
+        date: datetime,
+        amount: Money,
+        note_id: int,
+    ):
+        if amount == 0:
+            raise ValueError("Amount must be greater than 0")
+
+        if self.validate_note_id(note_id):
+            # return transaction if the transaction is already confirmed
+            for transaction in self.transactions:
+                if (
+                    transaction.transaction_id == transaction_id
+                    or transaction.note_id == note_id
+                ):
+                    return transaction
+
+            transaction = BankTransferTransaction(
+                transaction_id=transaction_id,
+                date=date,
+                amount=amount,
+                note_id=note_id,
+            )
+            self.transactions.append(transaction)
+            self.status = BankTransferDonationStatus.PAID
+
+            return transaction
--- domain/bank_transfer_donation_test.py
+++ domain/bank_transfer_donation_test.py
@@ -5,6 +5,7 @@ import pytest
 from domain.bank_transfer_donation import (
     BankTransferDonation,
     BankTransferDonationStatus,
+    BankTransferNote,
 )
 from domain.donee import Donee, DoneeType
 from domain.donor import Donor, DonorType
@@ -14,7 +15,7 @@ from domain.money import Money
 @pytest.fixture
 def my_donor() -> Donor:
     return Donor(
-        donor_type=DonorType.Individual,
+        donor_type=DonorType.INDIVIDUAL,
         email="email@mail,com",
         phone="0987654321",
         name1="John",
@@ -30,12 +31,47 @@ def my_donee() -> Donee:
     return Donee(
         ref_id=1,
         name="Covid-19 Relief Fund",
-        donee_type=DoneeType.Project,
+        donee_type=DoneeType.PROJECT,
         meta={"key": "value"},
     )
 
 
+@pytest.fixture
+def my_note() -> BankTransferNote:
+    return BankTransferNote(
+        id=1,
+        note="note",
+        amount=Money(amount=100, currency="THB"),
+        date=datetime.now(),
+    )
+
+
 class TestBankTransferDonation:
+    def test_create_bank_transfer_donation(my_donor, my_donee):
+        donor = my_donor
+        donee = my_donee
+
+        bank_transfer_donation = BankTransferDonation(
+            id=1,
+            donation_number="123456",
+            expected_amount=Money(amount=100, currency="USD"),
+            donor=donor,
+            donee=donee,
+        )
+
+        assert bank_transfer_donation.id == 1
+        assert bank_transfer_donation.donation_number == "123456"
+        assert bank_transfer_donation.expected_amount == Money(
+            amount=100, currency="USD"
+        )
+        assert bank_transfer_donation.donor == donor
+        assert bank_transfer_donation.donee == donee
+        assert bank_transfer_donation.notes == []
+        assert bank_transfer_donation.transactions == []
+        assert bank_transfer_donation.status == BankTransferDonationStatus.NEW
+        assert bank_transfer_donation.form_data == {}
+        assert bank_transfer_donation.meta == {}
+
     def test_add_note_to_bank_transfer_donation(my_donor, my_donee):
         donor = my_donor
         donee = my_donee
@@ -50,16 +86,40 @@ class TestBankTransferDonation:
 
         assert bank_transfer_donation.notes == []
         assert bank_transfer_donation.transactions == []
-        assert bank_transfer_donation.status == BankTransferDonationStatus.New
+        assert bank_transfer_donation.status == BankTransferDonationStatus.NEW
         assert bank_transfer_donation.form_data == {}
         assert bank_transfer_donation.meta == {}
 
         bank_transfer_donation.add_note(
-            "test", Money(amount=100, currency="THB"), datetime.now()
+            note="test",
+            amount=Money(amount=100, currency="THB"),
+            date=datetime.now(),
         )
-        assert bank_transfer_donation.status == BankTransferDonationStatus.Pending
+        assert bank_transfer_donation.status == BankTransferDonationStatus.PENDING
         assert len(bank_transfer_donation.notes) == 1
 
-    # TODO: Implement the test
-    def test_approve_bank_transfer_donation(my_donor, my_donee):
-        pass
+    def test_confirm_bank_transfer_donation(my_donor, my_donee, my_note):
+        donor = my_donor
+        donee = my_donee
+        note = my_note
+
+        bank_transfer_donation = BankTransferDonation(
+            id=1,
+            donation_number="123456",
+            expected_amount=Money(amount=100, currency="USD"),
+            donor=donor,
+            donee=donee,
+            notes=[note],
+        )
+
+        transaction = bank_transfer_donation.confirm_transaction(
+            transaction_id=1,
+            date=datetime.now(),
+            amount=Money(amount=100, currency="THB"),
+            note_id=1,
+        )
+        assert bank_transfer_donation.status == BankTransferDonationStatus.PAID
+        assert len(bank_transfer_donation.transactions) == 1
+        assert transaction.amount == Money(amount=100, currency="THB")
+        assert transaction.note_id == note.id
+        assert transaction.date is not None
